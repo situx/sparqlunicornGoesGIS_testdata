42fc1bf7a7c44b0ae0f2331898327cb609a81ebf
situx <situx@users.noreply.github.com>
2023-01-24 23:25:30 +0000

    Deploying to gh-pages from  @ de54aa1385cbec9a7d77c14f7fb6eff3a7c114e3 🚀

--- limes_classtree.js
+++ limes_classtree.js
@@ -807,12 +807,7 @@ var tree={
               "instancecount": 1
             }
           },
-          "from": {
-            "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 0,
-              "http://www.opengis.net/ont/geosparql#FeatureCollection": 1
-            }
-          }
+          "from": {}
         },
         "instancecount": 4
       },
@@ -1565,7 +1560,11 @@ var tree={
           "from": {
             "http://www.opengis.net/ont/geosparql#hasGeometry": {
               "instancecount": 0,
-              "http://archaeoinformatics.link/ontology#LegionaryCamp": 1
+              "http://archaeoinformatics.link/ontology#MilitaryCamp": 1
+            },
+            "http://www.w3.org/2000/01/rdf-schema#member": {
+              "instancecount": 0,
+              "http://www.opengis.net/ont/geosparql#GeometryCollection": 1
             }
           }
         },
