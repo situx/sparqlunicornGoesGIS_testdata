92f79dfa39ffa4b383abd6d55b14549e33e7ef48
situx <situx@users.noreply.github.com>
2023-03-29 12:08:14 +0000

    Deploying to gh-pages from  @ 10c4ff26052fa42a5b923fb980b98062e1ad05d6 🚀

---
+++ minivowl_result.js
@@ -0,0 +1,65 @@
+var minivowlresult={
+ "info": [
+  {
+   "description": "Created with pyowl2vowl (version 0.1) as part of the SPARQLing Unicorn QGIS Plugin"
+  }
+ ],
+ "nodes": [
+  {
+   "name": "Ontology",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#Ontology"
+  },
+  {
+   "name": "AnnotationProperty",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#AnnotationProperty"
+  },
+  {
+   "name": "Datatype",
+   "type": "class",
+   "uri": "http://www.w3.org/2000/01/rdf-schema#Datatype"
+  },
+  {
+   "name": "ObjectProperty",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#ObjectProperty"
+  },
+  {
+   "name": "DatatypeProperty",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#DatatypeProperty"
+  },
+  {
+   "name": "Class",
+   "type": "class",
+   "uri": "http://www.w3.org/2002/07/owl#Class"
+  },
+  {
+   "name": "FeatureCollection",
+   "type": "class",
+   "uri": "http://www.opengis.net/ont/geosparql#FeatureCollection"
+  },
+  {
+   "name": "GeometryCollection",
+   "type": "class",
+   "uri": "http://www.opengis.net/ont/geosparql#GeometryCollection"
+  },
+  {
+   "name": "MilitaryCamp",
+   "type": "class",
+   "uri": "http://archaeoinformatics.link/ontology#MilitaryCamp"
+  },
+  {
+   "name": "Point",
+   "type": "class",
+   "uri": "http://www.opengis.net/ont/sf#Point"
+  },
+  {
+   "name": "LegionaryCamp",
+   "type": "class",
+   "uri": "http://archaeoinformatics.link/ontology#LegionaryCamp"
+  }
+ ],
+ "links": []
+}
