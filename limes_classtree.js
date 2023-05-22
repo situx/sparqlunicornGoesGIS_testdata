56af080ab49db3be14d5ad8dd9038dd7b82f56d4
situx <situx@users.noreply.github.com>
2023-05-22 00:52:03 +0000

    Deploying to gh-pages from @ sparqlunicorn/sparqlunicornGoesGIS_testdata@abbd97ee4982407e9759c3a6ff68fc630cb5d6e4 🚀

--- limes_classtree.js
+++ limes_classtree.js
@@ -78,13 +78,19 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 7,
-              "http://archaeoinformatics.link/ontology#LegionaryCamp": 6
+              "instancecount": 49,
+              "http://archaeoinformatics.link/ontology#MilitaryCamp": 48,
+              "http://archaeoinformatics.link/ontology#LegionaryCamp": 1
             }
           },
-          "from": {}
+          "from": {
+            "http://www.w3.org/2000/01/rdf-schema#member": {
+              "instancecount": 0,
+              "http://www.w3.org/2004/02/skos/core#Collection": 1
+            }
+          }
         },
-        "instancecount": 10
+        "instancecount": 52
       },
       {
         "id": "http://www.opengis.net/ont/geosparql#SpatialObjectCollection",
@@ -122,18 +128,13 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 49,
-              "http://www.opengis.net/ont/sf#Point": 49
+              "instancecount": 144,
+              "http://www.opengis.net/ont/sf#Point": 104
             }
           },
-          "from": {
-            "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 0,
-              "http://www.w3.org/2004/02/skos/core#Collection": 1
-            }
-          }
+          "from": {}
         },
-        "instancecount": 52
+        "instancecount": 147
       },
       {
         "id": "http://lod.squirrel.link/data/limes/daicus/a6ec226a-3c2a-444a-9221-839201e1135f",
@@ -1612,7 +1613,7 @@ var tree={
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
               "instancecount": 0,
-              "http://www.opengis.net/ont/geosparql#GeometryCollection": 2
+              "http://www.opengis.net/ont/geosparql#GeometryCollection": 1
             }
           }
         },
@@ -1727,7 +1728,7 @@ var tree={
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
               "instancecount": 2,
-              "http://www.opengis.net/ont/geosparql#FeatureCollection": 1
+              "http://www.opengis.net/ont/geosparql#GeometryCollection": 1
             }
           },
           "from": {}
