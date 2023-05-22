8bdc67f1082e799a62af9e5bf046918fdb5b3115
situx <situx@users.noreply.github.com>
2023-05-22 01:52:43 +0000

    Deploying to gh-pages from @ sparqlunicorn/sparqlunicornGoesGIS_testdata@3c0f924110fff93d7ac00bdc3225fc208560fb7f 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -63,13 +63,6 @@ var minivowlresult={
  ],
  "links": [
   {
-   "source": 10,
-   "target": 9,
-   "valueTo": "hasGeometry",
-   "propertyTo": "class",
-   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
-  },
-  {
    "source": 8,
    "target": 9,
    "valueTo": "hasGeometry",
@@ -77,15 +70,22 @@ var minivowlresult={
    "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
   },
   {
+   "source": 10,
+   "target": 9,
+   "valueTo": "hasGeometry",
+   "propertyTo": "class",
+   "uriTo": "http://www.opengis.net/ont/geosparql#hasGeometry"
+  },
+  {
    "source": 6,
-   "target": 10,
+   "target": 8,
    "valueTo": "member",
    "propertyTo": "class",
    "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
   },
   {
    "source": 6,
-   "target": 8,
+   "target": 10,
    "valueTo": "member",
    "propertyTo": "class",
    "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
@@ -99,14 +99,14 @@ var minivowlresult={
   },
   {
    "source": 7,
-   "target": 10,
+   "target": 8,
    "valueTo": "member",
    "propertyTo": "class",
    "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
   },
   {
    "source": 7,
-   "target": 8,
+   "target": 10,
    "valueTo": "member",
    "propertyTo": "class",
    "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
