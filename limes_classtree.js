a20aaadfbb2e6372cfb5cc64d86f43de37f73ff3
situx <situx@users.noreply.github.com>
2023-05-23 00:08:35 +0000

    Deploying to gh-pages from @ sparqlunicorn/sparqlunicornGoesGIS_testdata@a5343eb83ec6e76f4b2e84884454d8095fd21e0e 🚀

--- limes_classtree.js
+++ limes_classtree.js
@@ -128,18 +128,13 @@ var tree={
               "instancecount": 1
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 49,
-              "http://www.opengis.net/ont/sf#Point": 49
+              "instancecount": 144,
+              "http://www.opengis.net/ont/sf#Point": 104
             }
           },
-          "from": {
-            "http://www.w3.org/2000/01/rdf-schema#member": {
-              "instancecount": 0,
-              "http://www.w3.org/2004/02/skos/core#Collection": 1
-            }
-          }
+          "from": {}
         },
-        "instancecount": 52
+        "instancecount": 147
       },
       {
         "id": "http://lod.squirrel.link/data/limes/daicus/a6ec226a-3c2a-444a-9221-839201e1135f",
