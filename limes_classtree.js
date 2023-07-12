ec0b0c46f2f3d1d10e14ab6185fd42b7f87cebf7
situx <situx@users.noreply.github.com>
2023-07-12 14:39:37 +0000

    Deploying to gh-pages from @ sparqlunicorn/sparqlunicornGoesGIS_testdata@2a8a7048e22c08a3c03a4c28b8630116af3ac476 🚀

--- limes_classtree.js
+++ limes_classtree.js
@@ -846,7 +846,7 @@ var tree={
           "from": {
             "http://www.w3.org/2000/01/rdf-schema#member": {
               "instancecount": 0,
-              "http://www.opengis.net/ont/geosparql#FeatureCollection": 1
+              "http://www.opengis.net/ont/geosparql#FeatureCollection": 2
             }
           }
         },
@@ -1612,7 +1612,7 @@ var tree={
             },
             "http://www.w3.org/2000/01/rdf-schema#member": {
               "instancecount": 0,
-              "http://www.opengis.net/ont/geosparql#GeometryCollection": 2
+              "http://www.opengis.net/ont/geosparql#GeometryCollection": 1
             }
           }
         },
