2db23cf57b2fee1b2118edf3e2e052b3fcacdc5e
situx <situx@users.noreply.github.com>
2023-11-04 18:40:50 +0000

    Deploying to gh-pages from @ sparqlunicorn/sparqlunicornGoesGIS_testdata@3a756fb8e7025b712fcd90ed064c734b4779de82 🚀

--- minivowl_result.js
+++ minivowl_result.js
@@ -64,14 +64,14 @@ var minivowlresult={
  "links": [
   {
    "source": 7,
-   "target": 9,
+   "target": 8,
    "valueTo": "member",
    "propertyTo": "class",
    "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
   },
   {
    "source": 7,
-   "target": 8,
+   "target": 9,
    "valueTo": "member",
    "propertyTo": "class",
    "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
@@ -85,14 +85,14 @@ var minivowlresult={
   },
   {
    "source": 6,
-   "target": 9,
+   "target": 8,
    "valueTo": "member",
    "propertyTo": "class",
    "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
   },
   {
    "source": 6,
-   "target": 8,
+   "target": 9,
    "valueTo": "member",
    "propertyTo": "class",
    "uriTo": "http://www.w3.org/2000/01/rdf-schema#member"
